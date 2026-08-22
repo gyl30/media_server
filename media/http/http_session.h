afc961f39cac01c5e97c2baf1a917aa933701ea7
gyl30 <im.ggyyll@gmail.com>
2026-08-22 08:11:58 +0800

    接入 GB28181 UDP PS-RTP 输出

--- media/http/http_session.h
+++ media/http/http_session.h
@@ -39,6 +39,7 @@ class http_session final : public std::enable_shared_from_this<http_session>
     void handle_hls(const boost::urls::url_view& target);
     void handle_whep(const std::vector<std::string>& segments);
     void handle_gb28181(const std::vector<std::string>& segments);
+    void handle_gb28181_output(const std::vector<std::string>& segments);
     void handle_whep_post(const std::vector<std::string>& segments);
     void handle_whep_delete(const std::vector<std::string>& segments);
     void wait_hls_playlist(std::string stream_name);
