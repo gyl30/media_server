3f82197f371428b331e12e38e1983b5db9160c0f
gyl30 <im.ggyyll@gmail.com>
2026-08-24 14:42:50 +0800

    抽出 RTSP 输出轨道支持判断

--- media/rtsp/rtsp_output_track.h
+++ media/rtsp/rtsp_output_track.h
@@ -10,6 +10,16 @@
 namespace media_server
 {
 
+[[nodiscard]] inline bool rtsp_output_track_supported(const media_track& track)
+{
+    return (track.kind == media_kind::video && (track.codec == codec_id::h264 || track.codec == codec_id::h265)) ||
+           (track.kind == media_kind::audio && (track.codec == codec_id::aac ||
+                                                (track.codec == codec_id::opus && track.clock_rate == 48'000 &&
+                                                 (track.channel_count == 1 || track.channel_count == 2) && track.codec_config.empty()) ||
+                                                ((track.codec == codec_id::g711a || track.codec == codec_id::g711u) && track.clock_rate == 8'000 &&
+                                                 track.channel_count == 1 && track.codec_config.empty())));
+}
+
 struct rtsp_output_track_description
 {
     media_track track;
