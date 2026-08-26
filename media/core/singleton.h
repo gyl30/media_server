41fd84e387fe7f74b51e61b94e41d405ff01305b
gyl30 <im.ggyyll@gmail.com>
2026-08-26 20:36:58 +0800

    main 显式初始化和销毁 singleton

--- media/core/singleton.h
+++ media/core/singleton.h
@@ -1,6 +1,10 @@
 #ifndef MEDIA_CORE_SINGLETON_H
 #define MEDIA_CORE_SINGLETON_H
 
+#include <exception>
+#include <memory>
+#include <utility>
+
 namespace media_server
 {
 template <typename T>
@@ -10,9 +14,36 @@ class singleton
     singleton() = delete;
     ~singleton() = delete;
 
+    template <typename... Args>
+    static void init(Args&&... args)
+    {
+        auto& value = storage();
+        if (value)
+        {
+            std::terminate();
+        }
+        value.reset(new T(std::forward<Args>(args)...));
+    }
+
     static T& instance()
     {
-        static T value;
+        auto& value = storage();
+        if (!value)
+        {
+            std::terminate();
+        }
+        return *value;
+    }
+
+    static void destroy() noexcept
+    {
+        storage().reset();
+    }
+
+   private:
+    static std::unique_ptr<T>& storage()
+    {
+        static std::unique_ptr<T> value;
         return value;
     }
 };
