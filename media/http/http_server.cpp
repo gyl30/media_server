0b3cea6f7923de1c43aa24c447e8c2196780c08c
gyl30 <im.ggyyll@gmail.com>
2026-09-04 14:36:54 +0800

    协程化 TCP 连接监听

--- media/http/http_server.cpp
+++ media/http/http_server.cpp
@@ -1,5 +1,8 @@
 #include <utility>
 
+#include <boost/asio/detached.hpp>
+#include <boost/asio/post.hpp>
+
 #include "media/http/http_server.h"
 #include "media/http/http_session.h"
 
@@ -7,18 +10,22 @@ namespace media_server
 {
 http_server::http_server(io_context_pool& workers, const config& config)
     : workers_(workers),
+      worker_(workers.next()),
       config_(config),
-      listener_(std::make_shared<tcp_listener>(workers, config.http_port, boost::asio::ip::make_address(config.bind_address)))
+      listener_(worker_.io(), config.http_port, boost::asio::ip::make_address(config.bind_address))
 {
 }
 
 void http_server::startup(boost::system::error_code& error)
 {
+    listener_.startup(error);
+    if (error)
+    {
+        return;
+    }
+
     const auto self = shared_from_this();
-    listener_->startup([self, this](boost::system::error_code ec, worker_context& worker, boost::asio::ip::tcp::socket socket)
-                       { on_accept(ec, worker, std::move(socket)); },
-                       {},
-                       error);
+    boost::asio::spawn(worker_.io(), [self](boost::asio::yield_context yield) { self->run(yield); }, boost::asio::detached);
 }
 
 void http_server::shutdown()
@@ -31,26 +38,38 @@ void http_server::shutdown()
         }
         closed_ = true;
     }
-    listener_->shutdown();
+
+    const auto self = shared_from_this();
+    boost::asio::post(worker_.io(), [self]() { self->safe_shutdown(); });
 }
 
-void http_server::on_accept(boost::system::error_code error, worker_context& worker, boost::asio::ip::tcp::socket socket)
+void http_server::run(boost::asio::yield_context yield)
 {
-    if (error)
+    boost::system::error_code error;
+    for (;;)
     {
-        shutdown();
-        return;
+        auto* worker = &workers_.next();
+        boost::asio::ip::tcp::socket socket(worker->io());
+        listener_.accept(socket, {}, yield, error);
+        if (error)
+        {
+            break;
+        }
+
+        std::scoped_lock lock(mutex_);
+        if (closed_)
+        {
+            boost::system::error_code close_error;
+            socket.close(close_error);
+            break;
+        }
+
+        auto session = std::make_shared<http_session>(*worker, std::move(socket), workers_, config_);
+        session->startup();
     }
 
-    std::scoped_lock lock(mutex_);
-    if (closed_)
-    {
-        boost::system::error_code close_error;
-        socket.close(close_error);
-        return;
-    }
-
-    auto session = std::make_shared<http_session>(worker, std::move(socket), workers_, config_);
-    session->startup();
+    shutdown();
 }
+
+void http_server::safe_shutdown() { listener_.shutdown(); }
 }    // namespace media_server
--- media/net/tcp_listener.cpp
+++ media/net/tcp_listener.cpp
@@ -1,107 +1,78 @@
 #include <utility>
 
-#include <boost/asio/post.hpp>
+#include <boost/asio/cancel_after.hpp>
 #include <boost/asio/error.hpp>
-#include <boost/asio/detached.hpp>
-#include <boost/system/error_code.hpp>
 
 #include "media/net/tcp_listener.h"
 
 namespace media_server
 {
 
-tcp_listener::tcp_listener(io_context_pool& workers, std::uint16_t port, boost::asio::ip::address bind_address)
-    : worker_(workers.next()), timer_(worker_.io()), acceptor_(worker_.io()), port_(port), bind_address_(std::move(bind_address)), workers_(workers)
+tcp_listener::tcp_listener(boost::asio::io_context& io, std::uint16_t port, boost::asio::ip::address bind_address)
+    : timer_(io), acceptor_(io), port_(port), bind_address_(std::move(bind_address))
 {
 }
 
-void tcp_listener::startup(accept_handler handler, std::chrono::milliseconds timeout, boost::system::error_code& ec)
+void tcp_listener::startup(boost::system::error_code& error)
 {
+    error.clear();
     if (bind_address_.is_unspecified())
     {
-        ec = boost::asio::error::invalid_argument;
+        error = boost::asio::error::invalid_argument;
         return;
     }
 
-    const boost::asio::ip::tcp::endpoint endpoint{
-        bind_address_,
-        port_,
-    };
-    acceptor_.open(endpoint.protocol(), ec);
-    if (!ec)
+    const boost::asio::ip::tcp::endpoint endpoint{bind_address_, port_};
+    acceptor_.open(endpoint.protocol(), error);
+    if (!error)
     {
-        acceptor_.set_option(boost::asio::socket_base::reuse_address(true), ec);
+        acceptor_.set_option(boost::asio::socket_base::reuse_address(true), error);
     }
-    if (!ec)
+    if (!error)
     {
-        acceptor_.bind(endpoint, ec);
+        acceptor_.bind(endpoint, error);
     }
-    if (!ec)
+    if (!error)
     {
-        acceptor_.listen(boost::asio::socket_base::max_listen_connections, ec);
+        acceptor_.listen(boost::asio::socket_base::max_listen_connections, error);
     }
-    if (ec)
+    if (error)
     {
+        boost::system::error_code close_error;
+        acceptor_.close(close_error);
+    }
+}
+
+void tcp_listener::accept(boost::asio::ip::tcp::socket& socket,
+                          std::chrono::milliseconds timeout,
+                          boost::asio::yield_context& yield,
+                          boost::system::error_code& error)
+{
+    error.clear();
+    if (timeout > std::chrono::milliseconds::zero())
+    {
+        acceptor_.async_accept(socket, boost::asio::cancel_after(timer_, timeout, yield[error]));
+        if (error == boost::asio::error::operation_aborted && !closed_)
+        {
+            error = boost::asio::error::timed_out;
+        }
         return;
     }
 
-    accept_handler_ = std::move(handler);
-    const auto self = shared_from_this();
-    boost::asio::spawn(worker_.io(), [self, timeout](boost::asio::yield_context yield) { self->accept_loop(timeout, yield); }, boost::asio::detached);
+    acceptor_.async_accept(socket, yield[error]);
 }
 
 void tcp_listener::shutdown()
 {
-    const auto self = shared_from_this();
-    boost::asio::post(acceptor_.get_executor(), [this, self]() { safe_shutdown(); });
-}
-
-void tcp_listener::accept_loop(std::chrono::milliseconds timeout, boost::asio::yield_context& yield)
-{
-    boost::system::error_code ec;
-    for (;;)
-    {
-        if (timeout > std::chrono::milliseconds::zero())
-        {
-            timer_.expires_after(timeout);
-            const auto self = shared_from_this();
-            timer_.async_wait([self, this](const boost::system::error_code& timer_error) { on_timeout(timer_error); });
-        }
-
-        auto* worker = &workers_.next();
-        boost::asio::ip::tcp::socket socket{worker->io()};
-        acceptor_.async_accept(socket, yield[ec]);
-        timer_.cancel();
-        if (ec)
-        {
-            if (accept_handler_)
-            {
-                accept_handler_(ec, *worker, std::move(socket));
-            }
-            break;
-        }
-
-        accept_handler_({}, *worker, std::move(socket));
-    }
-}
-
-void tcp_listener::on_timeout(const boost::system::error_code& ec)
-{
-    if (ec)
+    if (closed_)
     {
         return;
     }
+    closed_ = true;
 
-    accept_handler_(boost::asio::error::make_error_code(boost::asio::error::timed_out), worker_, boost::asio::ip::tcp::socket{worker_.io()});
-}
-
-void tcp_listener::safe_shutdown()
-{
-    accept_handler_ = {};
-    timer_.cancel();
-    boost::system::error_code ec;
-    acceptor_.cancel(ec);
-    acceptor_.close(ec);
+    boost::system::error_code error;
+    acceptor_.cancel(error);
+    acceptor_.close(error);
 }
 
 }    // namespace media_server
