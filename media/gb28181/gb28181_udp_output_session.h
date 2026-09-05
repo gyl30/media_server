5ef48980acb2484c710b59fc29585853f34221c9
gyl30 <im.ggyyll@gmail.com>
2026-09-05 11:41:12 +0800

    GB28181 UDP 输出使用 yield 协程传输

--- media/gb28181/gb28181_udp_output_session.h
+++ media/gb28181/gb28181_udp_output_session.h
@@ -1,25 +1,27 @@
 #ifndef MEDIA_GB28181_GB28181_UDP_OUTPUT_SESSION_H
 #define MEDIA_GB28181_GB28181_UDP_OUTPUT_SESSION_H
 
+#include <deque>
 #include <memory>
 #include <string>
 #include <vector>
 #include <optional>
 
 #include <boost/asio/ip/udp.hpp>
+#include <boost/asio/spawn.hpp>
 #include <boost/asio/steady_timer.hpp>
 
 #include "media/core/media_stream.h"
 #include "media/core/stream_registry.h"
 #include "media/gb28181/gb28181_types.h"
 #include "media/net/port_manager.h"
+#include "media/net/udp_yield_transport.h"
 
 namespace media_server
 {
 
 class worker_context;
 class gb28181_output_media;
-class udp_socket;
 
 class gb28181_udp_output_session final : public stream_session, public std::enable_shared_from_this<gb28181_udp_output_session>
 {
@@ -35,17 +37,11 @@ class gb28181_udp_output_session final : public stream_session, public std::enab
     void shutdown() override;
 
    private:
-    struct udp_socket_pair
-    {
-        std::shared_ptr<udp_socket> rtp;
-        std::shared_ptr<udp_socket> rtcp;
-        port_manager_impl::port_pair local_ports;
-    };
-
-    [[nodiscard]] std::optional<udp_socket_pair> prepare_udp_sockets(boost::asio::ip::address bind_address);
-    void shutdown_udp_sockets();
+    [[nodiscard]] std::optional<port_manager_impl::port_pair> prepare_udp_transports(boost::asio::ip::address bind_address);
+    void shutdown_udp_transports();
+    void run_rtp_write(boost::asio::yield_context yield);
+    void run_rtcp_sender(boost::asio::yield_context yield);
     void send_packet(std::vector<std::uint8_t> packet);
-    void schedule_rtcp();
     void safe_shutdown();
 
     worker_context& worker_;
@@ -56,9 +52,10 @@ class gb28181_udp_output_session final : public stream_session, public std::enab
     boost::asio::ip::address bind_address_;
     boost::asio::ip::udp::endpoint remote_rtp_endpoint_;
     boost::asio::ip::udp::endpoint remote_rtcp_endpoint_;
+    udp_yield_transport rtp_transport_;
+    udp_yield_transport rtcp_transport_;
     boost::asio::steady_timer rtcp_timer_;
-    std::shared_ptr<udp_socket> rtp_socket_;
-    std::shared_ptr<udp_socket> rtcp_socket_;
+    std::deque<std::shared_ptr<std::vector<std::uint8_t>>> write_queue_;
     std::optional<port_manager_impl::port_pair> local_ports_;
     std::shared_ptr<gb28181_output_media> media_;
     void* rtcp_sender_{};
