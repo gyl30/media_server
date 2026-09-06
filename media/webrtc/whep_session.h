02d2fa3328012b1d012ce9eb21e986055088746e
gyl30 <im.ggyyll@gmail.com>
2026-09-06 21:08:19 +0800

    精简 WHEP 会话关闭状态

--- media/webrtc/whep_session.h
+++ media/webrtc/whep_session.h
@@ -123,7 +123,6 @@ class whep_session final : public media_reader, public std::enable_shared_from_t
     std::uint64_t track_revision_{};
     bool tracks_ready_{};
     bool started_{};
-    bool closed_{};
 };
 
 }    // namespace media_server
